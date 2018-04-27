536570d3422a4174004bbfa8cafbfd61915aadad
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-04-27 11:57:36 -0700

    Upload completed solutions to exercises on scope, IIFE's and variable hoisting.

--- src/penguin.js
+++ src/penguin.js
@@ -1,7 +1,7 @@
 // Yikes, this code doesn't work at all. Make as few changes as possible
 // to the below code to get the correct statement to return.
 
-//Solution1L
+//Solution
 var penguin = function (name) {
     return 'Hello, my name is ' + name;
   }
@@ -17,8 +17,10 @@ module.exports = penguin
 
 //the problem here is that since penguin is a function expression instead of a function declaration, it can't be called before it's been declared like one would be able to do with a function declaration.  Could also have fixed the exercise by turning speak into an immediately invoked function and calling it
 
+
+//ORIGINAL CODE FROM EXERCISE THAT DOESN'T WORK:
 // Yikes, this code doesn't work at all. Make as few changes as possible
-// to the below code to get the correct statement to return. 	
+// to the below code to get the correct statement to return.
 
  // var penguin = function (name) {
  //   return penguin()
